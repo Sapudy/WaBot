ed1ef7ef4bcc8efed12ff66965d1e7a93aca7821
Sapudy <saputrasapudy@gmail.com>
2025-04-09 19:09:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -30,40 +30,40 @@ const readMore = more.repeat(4001)
 
 
 global.owner = [
-['628983894004', true],
-['628983894004', 'ZikoXd', true],
+['6281217222953', true],
+['6281217222953', 'Sapudy', true],
 ['628983894004', 'Zikoxd', true]
 ]
 global.mods = []
 global.prems = []
-global.nomorbot = '628983894004'
-global.nomorown = '628983894004'
+global.nomorbot = '6281226889408'
+global.nomorown = '6281217222953'
 global.nomorown1 = '628983894004'
 global.nomorown2 = '628983894004'
-global.nomorwa = '628983894004'
+global.nomorwa = '6281217222953'
 
 
 global.readMore = readMore
-global.author = 'ZikoXD' // author jangan di ubah hargai yang buat
-global.namebot = '​ZIKOXD - MD'
-global.wm = '© ZIKOXD By ZIKOXD'
+global.author = 'Sapudy' // author jangan di ubah hargai yang buat
+global.namebot = '​Chinatsu - MD'
+global.wm = '© Chinatsu By ZIKOXD'
 global.media = 'https://telegra.ph/file/02a8d3c4b00146bbfeb5d.jpg' // jangan di ubah kalo di ubah error
 global.watermark = wm
-global.wm2 = 'Zeltoria'
+global.wm2 = 'Chinatsu'
 global.botdate = `Date: ${week} ${date}\nTime: ${wktuwib}`
 global.bottime = `Time: ${wktuwib}`
-global.titlebot = `Zikoxd - MD`
-global.stickpack = 'Zikoxd By'
-global.stickauth = `© Zikoxd` // jangan di ubah kalo di ubah error
+global.titlebot = `Chinatsu - MD`
+global.stickpack = 'Chinatsu By'
+global.stickauth = `© Sapudy` // jangan di ubah kalo di ubah error
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
-global.nameown1= 'Zikoxd x Vald'
+global.nameown1= 'Sapudy'
 global.nameown2 = 'Zikoxd X Vald'
 global.Linkgc = 'Bentar'
 global.lynk = '-'
 //Link Social Media Ganti Aja Kalau Ga Punya Biarin Aja
 global.sig = 'https://www.instagram.com/bot' //instagram
-global.sgh = 'https://github.com/bot' //github
+global.sgh = 'https://github.com/Sapudy' //github
 global.sgc = 'https://chat.whatsapp.com/EWABTHINOO5CgpHXwSE9Iz' //group whatsapp
 global.sdc = '-' //discord
 global.snh = 'https://www.youtube.com/@bot' //youtube
@@ -178,12 +178,12 @@ global.rpg = {
 }
 
 // untuk apikey biarin aja pake ini aja kalau fitur error beli apikeynya
-global.uptime = '' // Masukin APIKEY uptimerobot kamu disini ===== https://uptimerobot.com/dashboard?ref=website-header#mySettings
+global.uptime = 'u2396619-45f9696f17b1e670e2d5d333' // Masukin APIKEY uptimerobot kamu disini ===== https://uptimerobot.com/dashboard?ref=website-header#mySettings
 global.openai = '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'  //api key bisa didapatkan dari https://openai.com/api/
 global.org = 'org-VKTbfzIGL0EfLb8RgRfbLvbu'  //openAI Organization name
 global.deepai = 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K' // https://deepai.org
 global.zenzkey = 'zenzkey_1ec92f71d3bb'
-global.lolkey = 'SGWN'
+global.lolkey = '692ea6e8d856d5a5a0782cb9'
 global.caliph = 'Lwlct6wi'
 global.rose = 'Rs-Danil_Elistz'
 global.zeltoria = 'Elistz'
@@ -199,7 +199,7 @@ global.APIs = {
 }
 
 global.APIKeys = { 
-  'https://api.lolhuman.xyz': 'SGWN',
+  'https://api.lolhuman.xyz': '692ea6e8d856d5a5a0782cb9',
   'https://violetics.pw': 'beta',
   'https://api.itsrose.site': 'Rs-Danil_Elistz',
   'https://api-xcoders.site': 'Frieren',
